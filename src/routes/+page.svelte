4c2f884675be1be23961b960da145a27aaf3f9dd
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-12-19 17:25:13 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<!-- remove `lang="ts"` and the errors are now accurate -->
+<script lang="ts">
+	let { something, tasty } = { something: "sweet", tasty: "treats" };
+
+	let pizza = "pepperoni";
+
+	const $props = 4;
+</script>
+
+<button>Cook</button>
